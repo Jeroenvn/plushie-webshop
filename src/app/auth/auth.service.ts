4bf4f11d7575e4906088e0cb47e936ddf2ba6749
Jeroen van Noort <s1137642@student.hsleiden.nl>
2025-10-25 16:17:33 +0200

    feat: login method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,14 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private httpClient = inject(HttpClient);
 
   signUp(username: String, password: String) {
     return this.httpClient.post('http://localhost:8081/auth/newUser', {
+      username: username,
+      password: password,
+    });
+  }
+
+  login(username: String, password: String) {
+    return this.httpClient.post('http://localhost:8081/auth/generateToken', {
       username: username,
       password: password,
     });
