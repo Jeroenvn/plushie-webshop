6bbf1895165f0257001ebc7f572776dd0ae11070
Jeroen van Noort <s1137642@student.hsleiden.nl>
2025-10-26 08:52:29 +0100

    change: added typing of response to requests and adding saving logged in user to the authService

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { catchError, Subject, tap, throwError } from 'rxjs';
+import { User } from './user.model';
 
 export interface AuthResponseData {
   token: string;
-  expiresIn: string;
+  expiresInSeconds: string;
+  userId: string;
 }
 
 @Injectable({
@@ -13,22 +15,44 @@ export interface AuthResponseData {
 export class AuthService {
   private httpClient = inject(HttpClient);
 
-  signUp(username: String, password: String) {
+  user = new Subject<User>();
+
+  signUp(username: string, password: string) {
     return this.httpClient
       .post<AuthResponseData>('http://localhost:8081/auth/newUser', {
         username: username,
         password: password,
       })
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap((responseData) => {
+          this.handleAuthentication(
+            username,
+            responseData.userId,
+            responseData.token,
+            responseData.expiresInSeconds
+          );
+        })
+      );
   }
 
-  login(username: String, password: String) {
+  login(username: string, password: string) {
     return this.httpClient
       .post<AuthResponseData>('http://localhost:8081/auth/generateToken', {
         username: username,
         password: password,
       })
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap((responseData) => {
+          this.handleAuthentication(
+            username,
+            responseData.userId,
+            responseData.token,
+            responseData.expiresInSeconds
+          );
+        })
+      );
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
@@ -38,5 +62,16 @@ export class AuthService {
     }
     errorMessage = errorResponse.error.message;
     return throwError(errorMessage);
+  }
+
+  private handleAuthentication(
+    username: string,
+    userId: string,
+    token: string,
+    expiresInSeconds: string
+  ) {
+    const expirationDate = new Date(new Date().getTime() + +expiresInSeconds * 1000);
+    const user = new User(username, userId, token, expirationDate);
+    this.user.next(user);
   }
 }
