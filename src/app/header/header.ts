73a598129e1eac26b69552a8e4324f333bf055e3
Jeroen van Noort <s1137642@student.hsleiden.nl>
2025-10-26 20:53:54 +0100

    feat: added subscription to user to update header display on authentication

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,17 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
-  isLoggedIn = false;
+export class Header implements OnInit {
+  private authService = inject(AuthService);
+
+  isAuthenticated = false;
   isAdmin = true;
+
+  ngOnInit(): void {
+    this.authService.user.subscribe(
+      (user) => (this.isAuthenticated = !!user)
+    );
+  }
 
   onLogout() {
     throw new Error('Method not implemented.');
