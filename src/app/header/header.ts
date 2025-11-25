0c191bd0dd6c8b3e183f939c85bf35c1abfa004c
Jeroen van Noort <s1137642@student.hsleiden.nl>
2025-11-25 11:53:50 +0100

    change: changed header to use primeng

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,30 +1,74 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
-import { Subscription } from 'rxjs';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [Menubar],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header implements OnInit {
+  private router = inject(Router);
   private authService = inject(AuthService);
 
+  items: MenuItem[] | undefined;
   isAuthenticated = false;
   isAdmin = false;
 
   ngOnInit(): void {
+    this.checkAuthStatus();
+  }
+
+  checkAuthStatus(){
     this.authService.user.subscribe((user) => {
       if (!!user) {
         this.isAuthenticated = true;
         this.isAdmin = user.isAdmin;
       }
+      this.updateMenuItems();
     });
   }
 
-  onLogout() {
-    this.authService.logout();
+  updateMenuItems(){
+    this.items = [];
+
+    this.items.push({
+      label: 'Products',
+      icon: 'pi pi-home',
+      command: () => {
+        this.router.navigate(['/products'])
+      },
+    });
+
+    if (!this.isAuthenticated){
+      this.items.push({
+        label: 'Login',
+        icon: 'pi pi-home',
+        command: () => {
+          this.router.navigate(['/auth']);
+        },
+      });
+    }
+    else{
+      this.items.push({
+        label: 'Cart',
+        icon: 'pi pi-home',
+        command: () => {
+          this.router.navigate(['/cart']);
+        },
+      });
+      this.items.push({
+        label: 'Logout',
+        icon: 'pi pi-home',
+        command: () => {
+          this.authService.logout();
+          this.router.navigate(['/auth'])
+        }
+      });
+    }
+
   }
 }
